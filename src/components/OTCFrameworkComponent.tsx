27f596cab892bd3e8b516448b6be0be09821e7de
Ssemaganda-George <ssemagandageorge480@gmail.com>
2025-11-26 13:54:07 +0300

    New changes

--- src/components/OTCFrameworkComponent.tsx
+++ src/components/OTCFrameworkComponent.tsx
@@ -180,6 +180,7 @@ export function OTCFramework() {
                         "Short courses",
                         "Academic programmes", 
                         "Seminars",
+                        
                         "Master classes",
                         "Webinars"
                       ].map((item, idx) => (
